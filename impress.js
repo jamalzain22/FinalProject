29016b8a13d07833ca9107a86c7b17b31dd6ce42
jamalzain22 <jama4650@mylaurier.ca>
2015-12-01 20:50:55 -0500

    Update impress.js

--- impress.js
+++ impress.js
@@ -1,3 +1,3 @@
 <div id="bored" class="step slide" data-x="-1000" data-y="-1500">
-        <q>Aren't you just <b>bored</b> with all those slides-based presentations?</q>
-    </div>
+<q>The <b>Turning</b> Test</q>
+</div>
