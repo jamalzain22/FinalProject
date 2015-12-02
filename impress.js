5d8c3e23e85bb7d9b876091551dc9f52022bfc9e
jamalzain22 <jama4650@mylaurier.ca>
2015-12-01 20:54:05 -0500

    Update impress.js

--- impress.js
+++ impress.js
@@ -1,3 +1,8 @@
 <div id="bored" class="step slide" data-x="-1000" data-y="-1500">
-<q>The <b>Turning</b> Test</q>
-</div>
+      <q>The Turning Test</q>
+      <img src="http://csunplugged.org/wp-content/uploads/2015/03/turing-test.jpg1246936875" 
+    </div>
+
+ <div class="step slide" data-x="0" data-y="-1500">
+        <q>What is The Turning Test?</q>
+    </div>
